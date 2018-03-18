7be328c3769b3436e68614cf9ecc4450cd54055f
Grant Southwood <grantmsouthwood@gmail.com>
2018-03-18 09:53:38 -0400

    tests

---
+++ SurfScraperTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SurfScraperTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SurfScraperTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f9ef5cc-97d8-4dcb-9f54-55f1dec866d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SurfScraperTests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SurfScraper.UtilityMethods;
+
+namespace SurfScraperTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]
+        public void PercentChecker()
+        {
+            
+            double result = Percent.DataAvailableVsPercentScraped(50, 0, 50);
+            Assert.AreEqual(100, result);
+
+            result = Percent.DataAvailableVsPercentScraped(50, 25, 50);
+            Assert.AreEqual(50, result);
+
+            result = Percent.DataAvailableVsPercentScraped(50, 2, 50);
+            Assert.AreEqual(96, result);
+
+            result = Percent.DataAvailableVsPercentScraped(80, 40, 80);
+            Assert.AreEqual(50, result);
+        }
+    }
+}
